35cee0116d992d079a66f588f6a76a10d64b8280
amitaysoffer <amitaysof@gmail.com>
2021-01-27 15:06:44 +0000

    added changes

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,17 +10,17 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Amitay Soffer',
+  subtitle: 'Full Stack Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'profile-pic.jpg',
+  paragraphOne: 'Not so long ago, I was about to start my masters in psychology in Israel. Until then, my working experience included mentoring and treating young adults in hospitals, boarding schools and special programs. As fulfilling as that was, I felt I am not doing what I\'\m supposed to do.It was around that time that I realized I needed a change.',
+  paragraphTwo: 'I spent the last several years working in the ad-tech industry, successfully managing and implementing various products whilst extensively using Javascript, HTML and CSS. About six months ago I left my comfort zone in order to pursue my dream of becoming a full time web developer. Since then I joined a unique coding mentorship program in which I am learning new things every single day.',
+  paragraphThree: 'Today I live in the UK, working hard every day to polish my skills and to write better and cleaner code. So, if you know of any exciting opportunities that you feel would be a good fit please feel free to let me know.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,7 +28,16 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'lepoint.png',
+    title: 'hey there',
+    info: 'rock star',
+    info2: 'baby',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'fantazy-stocks.png',
     title: '',
     info: '',
     info2: '',
@@ -37,7 +46,7 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'snake-game.png',
     title: '',
     info: '',
     info2: '',
@@ -46,7 +55,7 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'twitter-showcase.png',
     title: '',
     info: '',
     info2: '',
