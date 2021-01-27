d9cc9c3de6188fecd45ac7c0ad0abaac5613a638
amitaysoffer <amitaysof@gmail.com>
2021-01-27 20:52:29 +0000

    text fix 2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like together? Awesome!'}
+              {cta || 'Want to work together? Awesome!'}
             </p>
             <a
               target="_blank"
